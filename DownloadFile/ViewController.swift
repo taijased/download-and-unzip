73dc2189d80fa2dbabe7656967027316faeb4556
Maxim Spiridonov <taijased@gmail.com>
2019-03-18 14:33:57 +0300

    Initial Commit

---
+++ DownloadFile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DownloadFile
+//
+//  Created by Maxim Spiridonov on 18/03/2019.
+//  Copyright © 2019 Maxim Spiridonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
